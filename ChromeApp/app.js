39b241cbda2963b26a8e9314a6c25fbd5d212eea
정지후 <loveadsadf1976@gmail.com>
2022-02-06 19:45:05 +0900

    variable

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,1 +1,20 @@
-alert("Hello");
+// variable 변수
+// - 값을 저장하거나 유지하는 역할
+
+// variable 종류
+// - const : 바뀌지 않는 값 계속 유지된다.
+const a = 5;
+const b = 2;
+const myName = "jihoo";
+
+// variable 이름 짓기
+// - 공백이 있을 수 없다.
+// - 이름은 camelCase로 지어야한다.
+
+// console.log(5 + b);
+// console.log(5 * b);
+// console.log(5 / b);
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hi " + myName);
