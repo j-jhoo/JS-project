53ea501bba7c606eb06d46afa1db57842242692e
정지후 <loveadsadf1976@gmail.com>
2022-02-08 20:41:42 +0900

    NaN인지 구별하는 방법

--- ChromeApp/script.js
+++ ChromeApp/script.js
@@ -15,11 +15,21 @@
 
 // function -> 함수는 어떤 일을 수행하고 그 결과를 알려주는 것
 // return 키워드
-const age = 96;
-function calculaterKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
+// const age = 96;
+// function calculaterKrAge(ageOfForeigner) {
+//   return ageOfForeigner + 2;
+// }
 
-const krAge = calculaterKrAge(age);
+// const krAge = calculaterKrAge(age);
 
-console.log(krAge);
+// console.log(krAge);
+
+// NaN인지 구별하는 방법
+// const age = NaN;
+// // age는 사용자가 무엇을 입력하냐에 따라 number가 아닐 수 있다.
+// // isNaN은 boolean(true/false)를 반환한다.
+// // -> age가 숫자가 아니다!라는 뜻
+
+// if (true) {
+//   console.log("숫자 적어줘");
+// }
