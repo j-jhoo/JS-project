eb644b75e6760a8fd7923808e8b0d104011934f6
정지후 <loveadsadf1976@gmail.com>
2022-02-08 20:12:12 +0900

    함수에 정보를 보내는 법

--- ChromeApp/script.js
+++ ChromeApp/script.js
@@ -1,6 +1,16 @@
 // function
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
+// function plus(firstNumber, secondNumber) {
+//   console.log(firstNumber + secondNumber);
+// }
 
-plus(8, 60);
+// plus(8, 60);
+
+const player = {
+  name: "hoo",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you");
+  },
+};
+
+console.log(player.name);
+player.sayHello("coco");
