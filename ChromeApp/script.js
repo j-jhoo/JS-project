5322a0d65a9c078a2796efc13f948d0fc38abaad
정지후 <loveadsadf1976@gmail.com>
2022-02-08 20:23:33 +0900

    데이터타입 정리

--- ChromeApp/script.js
+++ ChromeApp/script.js
@@ -1,16 +1,14 @@
-// function
-// function plus(firstNumber, secondNumber) {
-//   console.log(firstNumber + secondNumber);
-// }
+// let -> 업데이트 가능
+// const -> 업데이트 불가능 (상수데이터)
+// :: const를 사용하되, 업데이트가 필요한 부분에는 let을 사용한다.
 
-// plus(8, 60);
+// variable - 데이터타입
+// 숫자 -> 1,2,3
+// 텍스트 -> "가", "나", "다"
+// boolean -> true/false
+// undefined -> 변수에 값을 부여하지 않은 상태 (값이 아무것도 없다. )
+// null -> 비어있음 (비어있음은 값이 있다)
 
-const player = {
-  name: "hoo",
-  sayHello: function (otherPersonsName) {
-    console.log("hello " + otherPersonsName + " nice to meet you");
-  },
-};
-
-console.log(player.name);
-player.sayHello("coco");
+// 많은 양의 데이터를 넣고 싶을 때
+// array
+// [1,2,3,4,false,null,undefined,'text']
