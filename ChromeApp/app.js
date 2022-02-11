a6e146f4770464bcb345637b3043385e95137a22
정지후 <loveadsadf1976@gmail.com>
2022-02-11 22:26:40 +0900

    className

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,5 +1,5 @@
 // html의 요소들은 javascript를 통해서 변경하고 읽을 수 있다.
-const title = document.getElementById("title");
+const title = document.getElementsByClassName("title");
 
 title.innerText = "Got you!";
 
