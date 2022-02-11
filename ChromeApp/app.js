9302f41455c3db84181714e9ceda4883a0cac54b
정지후 <loveadsadf1976@gmail.com>
2022-02-11 22:22:45 +0900

    js로 html변경하기

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,4 +1,7 @@
 // html의 요소들은 javascript를 통해서 변경하고 읽을 수 있다.
 const title = document.getElementById("title");
 
-console.log(title);
+title.innerText = "Got you!";
+
+console.log("title".id);
+console.log("title".class);
