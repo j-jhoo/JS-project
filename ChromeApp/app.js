605171639abd011a14082bb6a9c42e1b14b5fd1d
정지후 <loveadsadf1976@gmail.com>
2022-02-12 21:22:55 +0900

    querySelector

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,7 +1,6 @@
 // html의 요소들은 javascript를 통해서 변경하고 읽을 수 있다.
-const title = document.getElementsByClassName("title");
+// const title = document.getElementsByClassName("title");
+const title = document.querySelector(".title:first-child h1");
+// querySelector를 사용하여서 클래스, 아이디를 지정할 수 있고 그의 자식까지 선택할 수 있다.
 
 title.innerText = "Got you!";
-
-console.log("title".id);
-console.log("title".class);
