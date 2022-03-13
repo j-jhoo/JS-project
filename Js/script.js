e94809a873085043f8a623e0871d340da80c070e
정지후 <loveadsadf1976@gmail.com>
2022-03-14 00:13:08 +0900

    1round

---
+++ Js/script.js
@@ -0,0 +1,34 @@
+// let age = prompt("당신의 나이는??");
+
+// if (age < 26) {
+//   document.write("당신은 20대 초반입니까?");
+// } else {
+//   document.write("당신은 20대 중후반입니다. ");
+// }
+
+// //p.46
+// let num1 = 10;
+// let num2 = 5;
+
+// // num1 += num2;
+// // document.write(num1); // num1 = num1 + num2
+
+// num1 -= num2;
+// document.write(num2); // num1 = num1 - num2
+
+// p.55
+// let userHeight = 180;
+// let userWeight = 74;
+
+// const result = (userHeight - 100) * 0.9;
+// document.write(result);
+
+// // p.57
+// var name = prompt("이름은?", "");
+// let height = prompt("키는?", "0");
+// let weight = prompt("몸무게는?", "0");
+
+// let result = (height - 100) * 0.9;
+// let last = weight >= result - 5 && weight <= result + 5;
+// result = result ? "적정 체중입니다." : "적정 체중아님";
+// document.write(name + "님은" + result);
