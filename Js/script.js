8dc06955dd457ab56daeaec7522e3cd8edd34fc2
정지후 <loveadsadf1976@gmail.com>
2022-03-14 21:19:01 +0900

    첫째마당 mission

--- Js/script.js
+++ Js/script.js
@@ -32,3 +32,24 @@
 // let last = weight >= result - 5 && weight <= result + 5;
 // result = result ? "적정 체중입니다." : "적정 체중아님";
 // document.write(name + "님은" + result);
+
+// mission 1-1
+// let bus = 3000;
+// let food = 6000;
+// let drink = 3000;
+
+// let result = bus + food + drink;
+// let lastResult = 10000 < result ? result - 10000 + "원 초과" : "돈 관리 잘함";
+// document.write("진영님은" + lastResult);
+
+// // mission 1-2
+// const firth = 1200;
+// const second = 1300;
+// const third = 1000;
+// let avgSales = (firth + second + third) / 3;
+// const fourth = prompt("4분기 판매량은??", "0");
+// let msg1 = "판매량이 평균 이상입니다.";
+// let msg2 = "판매량이 평균 미달입니다.";
+
+// let result = fourth >= avgSales ? msg1 : msg2;
+// document.write(result);
